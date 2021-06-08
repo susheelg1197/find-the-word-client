375cefc801cc281957903151ffbdb55c52127696
Susheel Gounder <susheel.g@peopleinteractive.in>
2021-06-08 23:32:53 +0530

    Navigation to home

--- src/views/parts/HeaderBar.vue
+++ src/views/parts/HeaderBar.vue
@@ -1,7 +1,7 @@
 <template>
   <mdb-navbar expand="large" class="nav-b">
       <div class="container-fluid">
-      <h1 class="text-right">Find the Word!</h1>
+      <h1 @click="backToHome()" class="head-er text-right">Find the Word!</h1>
       </div>
     
   </mdb-navbar>
@@ -12,10 +12,18 @@
     name: 'NavbarPage',
     components: {
       mdbNavbar
+    },
+    methods:{
+      backToHome(){
+        window.open(window.location.origin,"_self")
+      }
     }
   }
 </script>
 <style scoped>
+.head-er{
+  cursor: pointer;
+}
 .nav-b{
     background-color:#5755d9 ;
     color:white;
